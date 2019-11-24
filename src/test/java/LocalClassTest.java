fd8a3e3ed56b3426c092759a659a792b6ca26e57
MikeGray31 <michaelkgray31@gmail.com>
2019-11-24 18:05:40 -0500

    Finished Lambda branch

--- src/test/java/LocalClassTest.java
+++ src/test/java/LocalClassTest.java
@@ -5,7 +5,13 @@ public class LocalClassTest {
     @Test
     public void AgeTestTest(){
         People people = new People();
-        People.printPersons(people.personList, new AgeTest());
+        CheckPerson checkThis = (Person p) -> {
+            if(p.age >= 21){
+                return true;
+            }
+            return false;
+        };
+        People.printPersons(people.personList, checkThis);
     }
 
 }
